a999c30f4edaf7d8cd49bab5dbc7f42b70dfdf7a
Andrew <cyberandrew30@yahoo.com>
2021-04-19 19:48:43 -0400

    updating contact port

--- contact.py
+++ contact.py
@@ -8,7 +8,8 @@ to_email = os.getenv("TO_EMAIL")
 
 
 def send_email(name, email, message):
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 587) as connection:
+        connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
                             to_addrs=to_email,
